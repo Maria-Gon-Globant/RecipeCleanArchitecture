ae7c056af9ee8b9c38c3e5ef80e21a295f0f2663
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-04-09 09:05:36 -0500

    Initial Commit

---
+++ Desserts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Desserts
+//
+//  Created by Maria Paula Gonzalez Escallon on 9/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Desserts/DessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DessertsApp.swift
+//  Desserts
+//
+//  Created by Maria Paula Gonzalez Escallon on 9/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
