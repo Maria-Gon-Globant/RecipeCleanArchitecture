1a107efb183545595b6198d78570641e890219ad
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-05-02 16:05:19 -0500

    Update: DessertsApp

--- Desserts/DessertsApp.swift
+++ Desserts/DessertsApp.swift
@@ -9,9 +9,41 @@ import SwiftUI
 
 @main
 struct DessertsApp: App {
+    var favoriteRepository = LocalFavoriteRepository()
+    var recipeRepository = LocalRecipeRepository()
+    var userRespository = LocalUserRepository()
+    @StateObject var recipeViewModel: RecipeViewModel
+    
+    init() {
+        let getRecipesUseCase = DefaultGetRecipesUseCase(recipeRepository: recipeRepository)
+        let getFavoritesUseCase = DefaultGetFavoriteUseCase(repository: favoriteRepository)
+        let addFavoriteRecipeUseCase = DefaultAddFavoriteUseCase(repository: favoriteRepository)
+        let removeFavoriteRecipeUseCase = DefaultRemoveFavoriteUseCase(repository: favoriteRepository)
+        let isFavoriteRecipeUseCase = DefaultIsFavoriteUseCase(repository: favoriteRepository)
+        let categoryRecipesUseCase = DefaultCategoryRecipesUseCase(getRecipesUseCase: getRecipesUseCase)
+        let filteredRecipeUseCase = DefaultFilteredRecipeUseCase(categoryRecipes: categoryRecipesUseCase)
+        let topFiveRecipesUseCase = DefaultTopFiveRecipesUseCase(categoryRecipes: categoryRecipesUseCase)
+        let getUserUseCase = DefaultGetUserUseCase(userRepository: userRespository)
+         
+        let viewModel = RecipeViewModel(
+            getRecipesUseCase: getRecipesUseCase,
+            getFavoritesRecipeUseCase: getFavoritesUseCase,
+            addFavoriteRecipeUseCase: addFavoriteRecipeUseCase,
+            removeFavoriteRecipeUseCase: removeFavoriteRecipeUseCase,
+            isFavoriteRecipeUseCase: isFavoriteRecipeUseCase, 
+            filteredRecipeUseCase: filteredRecipeUseCase,
+            categoryRecipesUseCase: categoryRecipesUseCase, 
+            topFiveRecipesUseCase: topFiveRecipesUseCase,
+            getUserUseCase: getUserUseCase
+        )
+        
+        _recipeViewModel = StateObject(wrappedValue: viewModel)
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(recipeViewModel)
         }
     }
 }
