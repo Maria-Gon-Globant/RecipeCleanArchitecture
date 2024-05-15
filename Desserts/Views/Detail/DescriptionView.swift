bb2d50b1c123cd029d101192b3fa835fb90f9daa
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-05-15 10:47:09 -0500

    Update: Toggle alignment

--- Desserts/Views/Detail/DescriptionView.swift
+++ Desserts/Views/Detail/DescriptionView.swift
@@ -12,48 +12,57 @@ struct DescriptionView: View {
     @State var isActive = false
     let recipe: Recipe
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(recipe.name)
-                .font(.title)
-                .fontWeight(.bold)
-            
-            HStack(spacing: 4){
-                ForEach(0 ..< Int(recipe.rate)) { _ in
-                    Image("star")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 25)
+        VStack{
+            VStack(alignment: .leading){
+                Text(recipe.name)
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                HStack(spacing: 4){
+                    ForEach(0 ..< Int(recipe.rate)) { _ in
+                        Image("star")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25)
+                    }
+                    
+                    Text("\(recipe.rate.round(num: 2))")
+                        .padding(.leading, 8)
+                    Spacer()
+                    
+                    Text(recipe.time)
                 }
-                Text("\(recipe.rate.round(num: 2))")
+                
+                Text("Description")
+                    .foregroundStyle(Color(hex: 0x070B9BE))
+                    .font(.system(size: 25))
+                    .bold()
+                    .fontWeight(.medium)
+                    .padding(.vertical, 8)
+                
+                Text(recipe.description)
+                    .opacity(0.8)
+                    .lineSpacing(8)
                     .padding(.leading, 8)
-                Spacer()
-                
-                Text(recipe.time)
             }
-            Text("Description")
-                .foregroundStyle(Color(hex: 0x070B9BE))
-                .font(.system(size: 25))
-                .bold()
-                .fontWeight(.medium)
-                .padding(.vertical, 8)
             
-            Text(recipe.description)
-                .opacity(0.8)
-                .lineSpacing(8)
-                .padding(.leading, 8)
+            VStack(alignment: .center){
+                Toggle("", isOn: $isActive)
+                    .toggleStyle(CustomToggleStyle())
+                    .padding()
+            }
             
+            VStack(alignment: .leading){
+                ForEach(isActive ? recipe.steps : recipe.ingredients, id: \.self) {
+                    str in
+                    Text("O  " + str)
+                    
+                }.multilineTextAlignment(.leading)
+                    .padding(.vertical, 5)
+                    .padding(.leading, 8)
+                    .padding(.trailing, 8)
+            }
             
-            Toggle("", isOn: $isActive)
-                .toggleStyle(CustomToggleStyle())
-                .padding()
-            ForEach(isActive ? recipe.steps : recipe.ingredients, id: \.self) {
-                str in
-                Text("O  " + str)
-                
-            }.multilineTextAlignment(.leading)
-                .padding(.vertical, 5)
-                .padding(.leading, 8)
-                .padding(.trailing, 8)
             Spacer(minLength: 50)
             
         }.padding()
