bc9d60643d7d66e67199f06871c98168e5f9e720
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-05-27 15:45:54 -0500

    Add: Is valid string use case

---
+++ Desserts/Domain/UseCases/RecipeUseCases/IsValidStringLengthUseCase/IsValidStringLengthUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  IsValidStringLengthUseCase.swift
+//  Desserts
+//
+//  Created by Maria Paula Gonzalez Escallon on 27/05/24.
+//
+
+import Foundation
+class IsValidStringLengthUseCase: IsValidStringLengthUseCaseProtocol {
+    func execute(str: String, maxLength: Int) -> Bool {
+        let count = str.filter{ $0 != "\n" }.count
+        return count >= 5 && count <= maxLength && str.isEmpty
+    }
+}
