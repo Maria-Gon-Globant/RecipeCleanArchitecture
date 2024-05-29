6471426f5ba13d4bb378c28f71f38485b5bfc5aa
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-05-29 09:46:11 -0500

    Add: Validate steps use case

---
+++ Desserts/Domain/UseCases/RecipeUseCases/ValidateStepsUseCase/ValidateStepsUseCase.swift
@@ -0,0 +1,27 @@
+//
+//  ValidateStepsUseCase.swift
+//  Desserts
+//
+//  Created by Maria Paula Gonzalez Escallon on 29/05/24.
+//
+
+import Foundation
+class ValidateStepsUseCase: ValidateStepsUseCaseProtocol {
+    
+    private let hasNewLine: HasNewLineUseCaseProtocol
+    private let isValidStringLength: IsValidStringLengthUseCaseProtocol
+    
+    init(hasNewLine: HasNewLineUseCaseProtocol, isValidStringLength: IsValidStringLengthUseCaseProtocol) {
+        self.hasNewLine = hasNewLine
+        self.isValidStringLength = isValidStringLength
+    }
+    
+    func execute(steps: String) -> String {
+        var rta: String = ""
+        
+        if !isValidStringLength.execute(str: steps, maxLength: 200) || !hasNewLine.execute(str: steps) {
+            rta = ErrorString.Steps.id
+        }
+        return rta
+    }
+}
---
+++ Desserts/Domain/UseCases/RecipeUseCases/ValidateStepsUseCase/ValidateStepsUseCaseProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  ValidateStepsUseCaseProtocol.swift
+//  Desserts
+//
+//  Created by Maria Paula Gonzalez Escallon on 29/05/24.
+//
+
+import Foundation
+protocol ValidateStepsUseCaseProtocol {
+    func execute(steps: String) -> String
+}
