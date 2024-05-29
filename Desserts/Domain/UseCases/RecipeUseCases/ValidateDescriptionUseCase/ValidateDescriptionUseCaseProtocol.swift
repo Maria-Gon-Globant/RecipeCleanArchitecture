d1c5b30f68320d70e8f95f391602978127846897
Maria-Gon-Globant <m.gonzalezescallon@globant.com>
2024-05-29 09:40:24 -0500

    Add: Validate description use case

---
+++ Desserts/Domain/UseCases/RecipeUseCases/ValidateDescriptionUseCase/ValidateDescriptionUseCaseProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  ValidateDescriptionUseCaseProtocol.swift
+//  Desserts
+//
+//  Created by Maria Paula Gonzalez Escallon on 29/05/24.
+//
+
+import Foundation
+protocol ValidateDescriptionUseCaseProtocol {
+    func execute(description: String) -> String
+}
